89b0ba6f5f480fa8a4fd8c574a4afc9872ba9dbe
abdiqaadir-araale <abdiqaadiraraale@gmail.com>
2026-09-19 22:29:30 +0300

    added  week1

---
+++ week1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    //Tags Php 
+    <?php
+    
+    //Echo - Display the output
+    echo "Welcome to PHP";
+    //use prenthesis is optional
+    echo ("Welcome to PHP");
+    //print also use the output
+    print "Welcome to Php";
+    // use prenthesis is optional
+    print ("Welcome to Php");
+
+    //Echo to display to argument without presenthesis 
+    echo "abdiqaadir" , "Araale"
+    
+
+    ?>
+
+    
+</body>
+</html>
